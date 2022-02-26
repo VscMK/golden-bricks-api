71c7146c75db9b5ab9de890679d85aab8eb4ca9d
Filip <filip_gosev@hotmail.com>
2022-02-26 13:09:48 +0100

    initial

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.listen(process.env.PORT, () =>
-  console.log(`App is listening on port ${process.env.PORT}!`)
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () =>
+    console.log(`App is listening on port ${PORT}!`)
 );
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Index route
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
+
 module.exports = {
-  app,
+    app,
 };
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/database');
+const User = require('../models/user');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+// Get users list
+router.get('/', (req, res) =>
+    User.findAll()
+    .then(users => res.render('users', {
+        users
+    }))
+    .catch(err => res.render('error', { error: err })));
