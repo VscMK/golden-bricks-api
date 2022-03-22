1db8f81e393579a3b9653aefd2bfa0eeea5a63f0
Filip <filip_gosev@hotmail.com>
2022-03-22 20:36:01 +0100

    add cors

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const path = require('path');
-
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors());
 
 const PORT = process.env.PORT || 8081;
 
@@ -17,6 +17,19 @@ app.listen(PORT, () =>
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes
 app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
