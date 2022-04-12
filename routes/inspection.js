7981e73b203d3eb3f83ac082721f007fd96bd6af
Filip <filip_gosev@hotmail.com>
2022-04-12 20:35:52 +0200

    excess code commented

--- routes/inspection.js
+++ routes/inspection.js
@@ -5,9 +5,9 @@ const Inspection = require('../controllers/inspection.controller');
 const checkAuth = require("../utils/check.auth");
 
 router.get('/', checkAuth, Inspection.findAll);
-router.post('/create', checkAuth, Inspection.create);
-router.put('/update/:id', checkAuth, Inspection.update);
-router.delete('/delete/:id', checkAuth, Inspection.delete);
+// router.post('/create', checkAuth, Inspection.create);
+// router.put('/update/:id', checkAuth, Inspection.update);
+// router.delete('/delete/:id', checkAuth, Inspection.delete);
 
 
 module.exports = router;
